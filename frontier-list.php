ce6b34e9a57b2164b9d42cdfff263bbbb4a5a81d
Scott Kingsley Clark <lol@scottkclark.com>
2014-04-21 03:11:06 -0500

    Fix default value for data; Reformat for WP code format standard

--- frontier-list.php
+++ frontier-list.php
@@ -1,20 +1,20 @@
 <?php
-
 // WP_List_Table is not loaded automatically so we need to load it in our application
-if( ! class_exists( 'WP_List_Table' ) ) {
+if ( !class_exists( 'WP_List_Table' ) ) {
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
 /**
  * Create a new table class that will extend the WP_List_Table for listing Frontier Elements
  */
-class Frontier_List_Table extends WP_List_Table{
+class Frontier_List_Table extends WP_List_Table {
+
 	/**
 	 * Prepare the items for the table to process
 	 *
 	 * @return Void
 	 */
-	public function prepare_items(){
+	public function prepare_items() {
 
 		$columns = $this->get_columns();
 		$hidden = $this->get_hidden_columns();
@@ -25,17 +25,18 @@ class Frontier_List_Table extends WP_List_Table{
 
 		$perPage = 20;
 		$currentPage = $this->get_pagenum();
-		$totalItems = count($data);
+		$totalItems = count( $data );
 
 		$this->set_pagination_args( array(
 			'total_items' => $totalItems,
-			'per_page'    => $perPage
-			) );
+			'per_page' => $perPage
+		) );
 
-		$data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
+		$data = array_slice( $data, ( ( $currentPage - 1 ) * $perPage ), $perPage );
 
-		$this->_column_headers = array($columns, $hidden, $sortable);
+		$this->_column_headers = array( $columns, $hidden, $sortable );
 		$this->items = $data;
+
 	}
 
 	/**
@@ -43,19 +44,20 @@ class Frontier_List_Table extends WP_List_Table{
 	 *
 	 * @return Array
 	 */
-	public function get_columns(){
+	public function get_columns() {
 
 		$columns = array(
 			//'cb'			=> '<input type="checkbox" />',
-			'id'			=> 'ID',
-			'title'			=> __('Title', 'pods-frontier'),
-			'description'	=> __('Description', 'pods-frontier'),
-			'type'			=> __('Type', 'pods-frontier'),
-			'shortcode'		=> __('Shortcode', 'pods-frontier'),
-			'base_pod'		=> __('Base Pod', 'pods-frontier')
-			);
+			'id' => 'ID',
+			'title' => __( 'Title', 'pods-frontier' ),
+			'description' => __( 'Description', 'pods-frontier' ),
+			'type' => __( 'Type', 'pods-frontier' ),
+			'shortcode' => __( 'Shortcode', 'pods-frontier' ),
+			'base_pod' => __( 'Base Pod', 'pods-frontier' )
+		);
 
 		return $columns;
+
 	}
 
 	/**
@@ -63,9 +65,10 @@ class Frontier_List_Table extends WP_List_Table{
 	 *
 	 * @return Array
 	 */
-	public function get_hidden_columns(){
+	public function get_hidden_columns() {
 
-		return array('id');
+		return array( 'id' );
+
 	}
 
 	/**
@@ -73,80 +76,82 @@ class Frontier_List_Table extends WP_List_Table{
 	 *
 	 * @return Array
 	 */
-	public function get_sortable_columns(){
+	public function get_sortable_columns() {
 
-		return array('title' => array('title', false),'type' => array('type', false));
+		return array( 'title' => array( 'title', false ), 'type' => array( 'type', false ) );
+
 	}
 
-	function column_cb($item) {
+	function column_cb( $item ) {
 
-		return sprintf(
-			'<input type="checkbox" name="book[]" value="%s" />', $item['id']
-		);    
+		return sprintf( '<input type="checkbox" name="book[]" value="%s" />', $item[ 'id' ] );
+
 	}
+
 	/**
 	 * Get the table data
 	 *
 	 * @return Array
 	 */
-	private function table_data(){
+	private function table_data() {
 
 		$data = array();
+
 		// get all elements
-		$elements = get_option('_pods_frontier_elements');
-		
+		$elements = (array) get_option( '_pods_frontier_elements', array() );
+
 		//get element types
-		$element_types = apply_filters('frontier_get_element_types', array() );
+		$element_types = apply_filters( 'frontier_get_element_types', array() );
 
-
-		foreach ($elements as $eid => $element) {
-
-			if(!isset($element_types[$element['type']])){
+		foreach ( $elements as $eid => $element ) {
+			if ( !isset( $element_types[ $element[ 'type' ] ] ) ) {
 				continue;
 			}
 
 			$data[] = array(
-				'id'			=> $eid,
-				'title'			=> $element['name'],
-				'description'	=> $element['description'],
-				'type'			=> $element_types[$element['type']]['name'],
-				'shortcode'		=> "[frontier id=\"" . $eid . "\"]",
-				'base_pod'		=> ( isset($element['base_pod']) ? $element['base_pod'] : __('Not Set', 'pods-frontier') )
-				);
-
+				'id' => $eid,
+				'title' => $element[ 'name' ],
+				'description' => $element[ 'description' ],
+				'type' => $element_types[ $element[ 'type' ] ][ 'name' ],
+				'shortcode' => '[frontier id="' . $eid . '"]',
+				'base_pod' => ( isset( $element[ 'base_pod' ] ) ? $element[ 'base_pod' ] : __( 'Not Set', 'pods-frontier' ) )
+			);
 		}
 
 		return $data;
+
 	}
 
 	/**
 	 * Define what data to show on each column of the table
 	 *
-	 * @param  Array $item        Data
+	 * @param  Array $item Data
 	 * @param  String $column_name - Current column name
 	 *
 	 * @return Mixed
 	 */
-	public function column_default( $item, $column_name ){
+	public function column_default( $item, $column_name ) {
 
-		switch( $column_name ) {
+		switch ( $column_name ) {
 			case 'id':
 			case 'title':
 				$out = $item[ $column_name ];
 				$out .= '<div class="row-actions">';
-				$out .= '<span class="edit"><a title=" ' . __('Edit this element', 'pods-frontier') . '" href="admin.php?page=pods-component-frontier&edit=' . $item['id'] . '">' . __('Edit', 'pods-frontier') . '</a> | </span>';
-				$out .= '<span class="trash"><a href="admin.php?page=pods-component-frontier&delete=' . $item['id'] . '&_pfnonce=' . wp_create_nonce( 'delete_frontier_element' ) . '" title="' . __('Move this item to the Trash', 'pods-frontier') . '" class="submitdelete">' . __('Delete', 'pods-frontier') . '</a></span>';
+				$out .= '<span class="edit"><a title=" ' . __( 'Edit this element', 'pods-frontier' ) . '" href="admin.php?page=pods-component-frontier&edit=' . $item[ 'id' ] . '">' . __( 'Edit', 'pods-frontier' ) . '</a> | </span>';
+				$out .= '<span class="trash"><a href="admin.php?page=pods-component-frontier&delete=' . $item[ 'id' ] . '&_pfnonce=' . wp_create_nonce( 'delete_frontier_element' ) . '" title="' . __( 'Move this item to the Trash', 'pods-frontier' ) . '" class="submitdelete">' . __( 'Delete', 'pods-frontier' ) . '</a></span>';
 				$out .= '</div>';
-			return $out;
+
+				return $out;
 			case 'description':
 			case 'type':
 			case 'shortcode':
 			case 'base_pod':
-			return $item[ $column_name ];
+				return $item[ $column_name ];
 
 			default:
-			return print_r( $item, true ) ;
+				return print_r( $item, true );
 		}
+
 	}
 
 	/**
@@ -154,51 +159,30 @@ class Frontier_List_Table extends WP_List_Table{
 	 *
 	 * @return Mixed
 	 */
-	private function sort_data( $a, $b ){
+	private function sort_data( $a, $b ) {
 
 		// Set defaults
 		$orderby = 'title';
 		$order = 'asc';
 
 		// If orderby is set, use this as the sort column
-		if(!empty($_GET['orderby']))
-		{
-			$orderby = $_GET['orderby'];
+		if ( !empty( $_GET[ 'orderby' ] ) ) {
+			$orderby = $_GET[ 'orderby' ];
 		}
 
 		// If order is set use this as the order
-		if(!empty($_GET['order']))
-		{
-			$order = $_GET['order'];
+		if ( !empty( $_GET[ 'order' ] ) ) {
+			$order = $_GET[ 'order' ];
 		}
 
+		$result = strcmp( $a[ $orderby ], $b[ $orderby ] );
 
-		$result = strcmp( $a[$orderby], $b[$orderby] );
-
-		if($order === 'asc')
-		{
+		if ( $order === 'asc' ) {
 			return $result;
 		}
 
 		return -$result;
+
 	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
