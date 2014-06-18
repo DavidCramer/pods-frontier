70dc26f47b986cc7b8dc68a577b0ae2d591eb7dc
David Cramer <david@digilab.co.za>
2014-06-18 19:26:35 +0200

    corrected reference

--- includes/element-view_template.php
+++ includes/element-view_template.php
@@ -14,7 +14,7 @@
 	<textarea id="css-editor-input" name="view_template[css]"><?php if( isset( $atts['css'] ) ){ echo htmlentities( $atts['css'] ); } ?></textarea>
 </div>
 <div id="js-editor" class="template-editor-wrap" style="display:none;">
-	<textarea id="js-editor-input" name="view_template[js]"><?php if( isset( $atts['css'] ) ){ echo htmlentities( $atts['js'] ); } ?></textarea>
+	<textarea id="js-editor-input" name="view_template[js]"><?php if( isset( $atts['js'] ) ){ echo htmlentities( $atts['js'] ); } ?></textarea>
 </div>
 <input type="hidden" id="editor_height" name="view_template[height]" value="<?php if( isset( $atts['height'] ) ){ echo sanitize_text_field( $atts['height'] ); } ?>">
 <input type="hidden" id="editor_tab" name="view_template[tab]" value="<?php if( isset( $atts['tab'] ) ){ echo sanitize_text_field( $atts['tab'] ); }else{ echo 'htmltab'; } ?>">
